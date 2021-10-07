74bf68adf95d5f35279358851ceb7077b190a4c7
allpick2 <88521113@qq.com>
2021-10-07 12:55:52 +0800

    完善挂机奖励功能
    完善挂机奖励功能

--- Assets/Script/NewBehaviourScript.cs
+++ Assets/Script/NewBehaviourScript.cs
@@ -10,10 +10,9 @@ public class NewBehaviourScript : MonoBehaviour
     public Text Text_NumYouHave, Text_GiveYouJingyanping, Text_LevelView;
     public Text Text_DatetimeBetween, Text_DatetimeFront, Text_DatetimeAfter;
     //增加经验和葡萄的变量
-    int Jingyan, Levelnum, JingyanNeed;
-    //int escapeTimes = 1;
+    int  Levelnum, JingyanNeed;
     long DatetimeFront, DatetimeAfter, DatetimeBetween ;
-    long Jingyanping;
+    long Jingyan, Jingyanping;
     long OfflineSec;
 
 
@@ -33,6 +32,14 @@ public class NewBehaviourScript : MonoBehaviour
         }
     }
 
+    //退出按钮
+    public void Button_QuitGame()
+    {
+        Save_F();
+        SaveDatetimeFront();
+        Application.Quit();
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -43,31 +50,17 @@ public class NewBehaviourScript : MonoBehaviour
         ViewDatetimeBetween();
         OfflineRewards();
         InvokeRepeating("Time_F", 0, 1f);
+        InvokeRepeating("Button_AddExp_F", 0, 1f);
     //打开游戏就显示信息
         Text_GiveYouJingyanping.text = "自动分发：当前你拥有经验瓶" + Jingyanping;
         Text_LevelView.text = "等级：" + Levelnum + "(" + Jingyan +"/" + JingyanNeed +")";
         Text_NumYouHave.text = "修炼中";
     }
 
-    void Time_F()
-    {
-        Jingyanping = Jingyanping + 1;
-        Text_GiveYouJingyanping.text = "自动分发：当前你拥有经验瓶" + Jingyanping;
-    }
-
-    //退出按钮
-    public void Button_QuitGame()
-    {
-        Save_F();
-        SaveDatetimeFront();
-        Application.Quit();
-    }
-
-
-
     // Update is called once per frame
     void Update()
     {
+        //左滑退出的功能
         if (Input.GetKey(KeyCode.Escape))
         {
             //弹出提示
@@ -82,20 +75,24 @@ public class NewBehaviourScript : MonoBehaviour
 
             }
         }
+        //有累计经验的情况下快速增长经验
+        if (Jingyan > JingyanNeed)
+        {
+            Button_AddExp_F();
+        }
     }
 
-    //IEnumerator resetTimes()
-    //{
-    //    yield return new WaitForSeconds(2);
-    //    escapeTimes = 1;
-    //}
+    void Time_F()
+    {
+        Jingyanping = Jingyanping + 1;
+        Text_GiveYouJingyanping.text = "自动分发：当前你拥有经验瓶" + Jingyanping;
+    }
 
-   
     //保存和读取功能
     void Save_F()
     {
         PlayerPrefs.SetString("Jingyanping", Convert.ToString(Jingyanping));
-        PlayerPrefs.SetInt("Jingyan", Jingyan);
+        PlayerPrefs.SetString("Jingyan", Convert.ToString(Jingyan));
         PlayerPrefs.SetInt("Levelnum", Levelnum);
         PlayerPrefs.SetInt("JingyanNeed", JingyanNeed);
         PlayerPrefs.Save();
@@ -103,10 +100,11 @@ public class NewBehaviourScript : MonoBehaviour
     void Load_F()
     {
         Jingyanping = Convert.ToInt64(PlayerPrefs.GetString("Jingyanping", "0"));
-        Jingyan = PlayerPrefs.GetInt("Jingyan", 0);
+        Jingyan = Convert.ToInt64(PlayerPrefs.GetString("Jingyan", "0"));
         Levelnum = PlayerPrefs.GetInt("Levelnum", 1);
         JingyanNeed = PlayerPrefs.GetInt("JingyanNeed", 1);
     }
+
     //等级提升显示
     void Levelup_F()
     {
@@ -127,7 +125,7 @@ public class NewBehaviourScript : MonoBehaviour
     void OfflineRewards()
     {
         //把不在线的时间加到经验瓶里去
-        Jingyanping = Jingyanping + OfflineSec;
+        Jingyan = Jingyan + OfflineSec;
         
         
     }
@@ -146,7 +144,7 @@ public class NewBehaviourScript : MonoBehaviour
 
     void GetDatetimeFront()
     {
-        string DatetimeFront = PlayerPrefs.GetString("DatetimeFront", "1533521300");
+        string DatetimeFront = PlayerPrefs.GetString("DatetimeFront", "88521113");
         Text_DatetimeFront.text = "上次离线时间：" + Convert.ToInt64(DatetimeFront);
     }
 
@@ -162,11 +160,14 @@ public class NewBehaviourScript : MonoBehaviour
 
     void ViewDatetimeBetween()
     {
-        string DatetimeFront = PlayerPrefs.GetString("DatetimeFront", "1533521300");
-        string DatetimeAfter = PlayerPrefs.GetString("DatetimeAfter", "1533521300");
+        //用于抵消第一次安装时的上次登录时间，改为现在
+        DateTime _time = DateTime.Now;
+        TimeSpan timeSpan = _time.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        long TimeNow = Convert.ToInt64(timeSpan.TotalSeconds) - 3;
+        string DatetimeFront = PlayerPrefs.GetString("DatetimeFront", Convert.ToString(TimeNow));
+        string DatetimeAfter = PlayerPrefs.GetString("DatetimeAfter", "88521113");
         long DatetimeBetween = Convert.ToInt64(DatetimeAfter) - Convert.ToInt64(DatetimeFront);
         Text_DatetimeBetween.text = "秒数相差：" + Convert.ToInt64(DatetimeBetween);
         OfflineSec = Convert.ToInt64(DatetimeBetween);
     }
-
 }
